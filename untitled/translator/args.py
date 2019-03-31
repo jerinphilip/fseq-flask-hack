b975e883a3fa5ecfc8a61873fad882104454b480
Jerin Philip <jerin.philip@students.iiit.ac.in>
2019-03-31 13:33:38 +0530

    Some more organization changes:

---
+++ untitled/translator/args.py
@@ -0,0 +1,31 @@
+
+from collections import namedtuple
+
+class Args:
+    def __init__(self, **kwargs):
+        self.custom_set = set()
+        self.enhance(**kwargs)
+
+    def __getattr__(self, key):
+        return self.__dict__.get(key, None)
+
+    def enhance(self, **kwargs):
+        for key, val in kwargs.items():
+            if key not in self.__dict__:
+                self.custom_set.add(key)
+                self.__dict__[key] = val
+
+    def __str__(self):
+        lines = []
+        for key in sorted(list(self.custom_set)):
+            line = '{key} : {val}'.format(key=key, val=self.__dict__[key])
+            lines.append(line)
+        return '\n'.join(lines)
+
+
+ckpt = 'checkpoints/mm-new.ckpt'
+args = Args( 
+    path=ckpt, max_tokens=1000, task='translation',
+    source_lang='src', target_lang='tgt', buffer_size=2,
+    data=['data/mm-all/']
+)
